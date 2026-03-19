d17491895abbbb47ffcdb8cd4533e80f82809288
John Hilary <john.h@codelynks.com>
2026-03-19 16:33:44 +0530

    feat:Added access token and refresh token api client to get profile

--- composeApp/src/commonMain/kotlin/com/john/kmpapplication/ui/login/LoginUiEvent.kt
+++ composeApp/src/commonMain/kotlin/com/john/kmpapplication/ui/login/LoginUiEvent.kt
@@ -1,6 +1,7 @@
 package com.john.kmpapplication.ui.login
 
 sealed interface LoginUiEvent {
+
     data class OnEmailChanged(val email: String) : LoginUiEvent
     data class OnPasswordChanged(val password: String) : LoginUiEvent
     data class OnLoginButtonClick(val email: String, val password: String) : LoginUiEvent
