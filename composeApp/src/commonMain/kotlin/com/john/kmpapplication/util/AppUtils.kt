0fcc9ddc8efa34f06bdaa3b28b6adb9c7310414e
John Hilary <john.h@codelynks.com>
2026-03-20 18:39:08 +0530

    Code cleanup

--- composeApp/src/commonMain/kotlin/com/john/kmpapplication/util/AppUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.john.kmpapplication.util
-
-
-object AppUtils {
-    fun String.isValidName(): Boolean {
-        return this.isNotBlank() && this.matches(Regex("^[a-zA-Z ]{2,}$"))
-    }
-
-}
